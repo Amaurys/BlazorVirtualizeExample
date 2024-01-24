bab6e86e519a5aeee3c1a0de77b5832304b9a1b2
Amaurys Sanchez <20170327@unapec.edu.do>
2024-01-24 19:42:54 -0400

    Add project files.

---
+++ BlazorVirtualizeProject/Models/Car.cs
@@ -0,0 +1,9 @@
+﻿namespace BlazorVirtualizeProject.Models
+{
+    public class Car
+    {
+        public Guid Id { get; set; }
+        public string? Name { get; set; }
+        public int Cost { get; set; }
+    }
+}
---
+++ BlazorVirtualizeProject/Pages/VirtualizeExamplePage.razor.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Components.Web.Virtualization;
+using Microsoft.AspNetCore.Components;
+using BlazorVirtualizeProject.Models;
+
+namespace BlazorVirtualizeProject.Pages
+{
+    public partial class VirtualizeExamplePage
+    {
+        const int OverscanCount = 3; 
+        public EventCallback LoadCarsEvent { get; set; }
+
+        protected override Task OnInitializedAsync()
+        {
+            LoadCarsEvent.InvokeAsync();
+            return base.OnInitializedAsync();
+        }
+
+        public async Task AnswerASelected()
+        {
+            await MakeCars();
+        }      
+
+        private static async Task<List<Car>> MakeCars()
+        {
+            List<Car> myCarList = new();
+
+            for (int i = 0; i < 10000; i++)
+            {
+                var car = new Car()
+                {
+                    Id = Guid.NewGuid(),
+                    Name = $"Car {i}",
+                    Cost = i * 77 + 105
+                };
+                myCarList.Add(car);
+            }
+            return await Task.FromResult(myCarList);
+        }
+
+        private static async ValueTask<ItemsProviderResult<Car>> LoadCars(ItemsProviderRequest request)
+        {
+            var cars = await MakeCars();            
+            await Task.Delay(10);
+            return new ItemsProviderResult<Car>(cars.Skip(request.StartIndex).Take(request.Count), cars.Count);
+        }
+    }
+}
